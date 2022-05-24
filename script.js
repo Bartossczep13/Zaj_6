221b09fa357f0fc535a2c09de19ebf4dbbffd268
TeO <100691829+Bartossczep13@users.noreply.github.com>
2022-05-24 11:40:49 +0000

    1_2

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 (function () {
-   const b = document.getElementById('ex1_content')
-   const c = document.getElementById('ex1_button')
-
-  c.addEventListener("click",function (){
-  var data="0,1,2,3,4,5,6,7,8,9";
- 
-    b.innerHTML=data;
-  })
+   const b = document.getElementById('ex2_content');
+   var c = document.getElementById('ex2_text');
+  
+  c.addEventListener("input",function (){
+  
+  if(c.value.match(/^[0-9]+$/) && c.value.length ==9){
+    b.textContent = "numer jest poprawny";
+  };
+    
 })();
