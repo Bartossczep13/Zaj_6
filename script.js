fc4258c67476ffdbb02c5d1dc6ac4002591f0f7f
TeO <100691829+Bartossczep13@users.noreply.github.com>
2022-05-24 10:51:16 +0000

    1_inne

--- script.js
+++ script.js
@@ -3,13 +3,8 @@
    const c = document.getElementById('ex1_button')
 
   c.addEventListener("click",function (){
-  var data="";
-    for(var i=0;i<10;i++)
-      {
-        if(i!=0)
-          data +=",";
-        data +=i.toString();
-      }
+  var data="0,1,2,3,4,5,6,7,8,9";
+ 
     b.innerHTML=data;
   })
 })();
