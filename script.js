18d2a20073a5f9623181b128418fc8371963eafe
TeO <100691829+Bartossczep13@users.noreply.github.com>
2022-05-24 10:47:53 +0000

    1_dziala

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    const b = document.getElementById('ex1_content')
    const c = document.getElementById('ex1_button')
 
-  b.addEventListener("click",function (){
+  c.addEventListener("click",function (){
   var data="";
     for(var i=0;i<10;i++)
       {
@@ -10,6 +10,6 @@
           data +=",";
         data +=i.toString();
       }
-    c.innerHTML=data;
+    b.innerHTML=data;
   })
 })();
