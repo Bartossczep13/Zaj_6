ff83cb9b55599e1f579d46a8600bfc76056c9d6c
TeO <100691829+Bartossczep13@users.noreply.github.com>
2022-05-24 10:44:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+(function () {
+   const b = document.getElementById('ex1_content')
+   const c = document.getElementById('ex1_button')
+
+  c.addEventListener("click",function (){
+  var data="";
+    for(var i=0;i<10;i++)
+      {
+        if(i!=0)
+          data+=",";
+        data +=i.toString();
+      }
+    b.innerHTML=data;
+  })
+})();
